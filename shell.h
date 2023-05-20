51bc1f42602bbbc6306f8a5aa707d91292cc9d7c
Listus97 <122838679+Listus97@users.noreply.github.com>
2023-05-20 22:52:22 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -17,6 +17,16 @@
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
+/* for command chaining */
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
+
+/* for convert_number() */
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
 /* 1 if using system getline() */
 #define USE_GETLINE 0
 #define USE_STRTOK 0
@@ -26,6 +36,11 @@
 #define USE_GETLINE 0
 #define USE_STRTOK 0
 #define CMD_NORM 0
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+
+extern char **environ;
 
 /**
  * struct liststr - a node in a singly linked list
